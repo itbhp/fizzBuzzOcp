ae66b5577849606c81566b3f224cd79ec6d5fa07
Paolo Sciarra <itbhps@gmail.com>
2017-12-27 12:45:45 +0100

    refactored test: renamed cases

--- src/test/java/it/twinsbrain/dojos/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/it/twinsbrain/dojos/fizzbuzz/FizzBuzzTest.java
@@ -2,7 +2,6 @@ package it.twinsbrain.dojos.fizzbuzz;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -13,25 +12,34 @@ import static org.junit.Assert.assertThat;
 public class FizzBuzzTest {
 
   @Test
-  public void oneAsString() throws Exception {
-    assertThat(FizzBuzz.of(1), is("1"));
+  @Parameters({
+    "1, 1",
+    "2, 2",
+    "4, 4",
+    "7, 7",
+    "8, 8",
+    "11, 11",
+    "91, 91"
+  })
+  public void simpleStrings(int sample, String adapted) throws Exception {
+    assertThat(FizzBuzz.of(sample), is(adapted));
   }
 
   @Test
   @Parameters({"3", "6", "9", "12", "18", "21", "24", "93"})
-  public void threeIsFizz(int sample) throws Exception {
+  public void fizzes(int sample) throws Exception {
     assertThat(FizzBuzz.of(sample), is("Fizz"));
   }
 
   @Test
   @Parameters({"5", "10", "20", "25"})
-  public void fiveAsBuzz(int sample) throws Exception {
+  public void buzzes(int sample) throws Exception {
     assertThat(FizzBuzz.of(sample), is("Buzz"));
   }
 
   @Test
   @Parameters({"15", "30", "45", "60", "90"})
-  public void fifteenIsFizzBuzz(int sample) throws Exception {
+  public void fizzBuzzes(int sample) throws Exception {
     assertThat(FizzBuzz.of(sample), is("FizzBuzz"));
   }
 }
